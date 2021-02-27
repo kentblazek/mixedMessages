8acabee851d3e3188e3091e3ecb75ff70da35f52
Kent Blazek <79373746+kentblazek@users.noreply.github.com>
2021-02-27 11:17:11 -0500

    Create empty source arrays

---
+++ headlineGenerator.js
@@ -0,0 +1,10 @@
+// Headline generator master object
+const phraseMaker = {
+    who: [],
+    whatAction: [],
+    whatAdj: [],
+    whatObject: [],
+    where: [],
+    whoReact: [],
+    reaction: [], 
+}
