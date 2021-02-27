9e457f1eed68afb0875f2237d999af359cf6c6ee
Kent Blazek <79373746+kentblazek@users.noreply.github.com>
2021-02-27 11:52:16 -0500

    create initial arrays

--- headlineGenerator.js
+++ headlineGenerator.js
@@ -1,7 +1,17 @@
 // Headline generator master object
 const phraseMaker = {
-    who: [],
-    whatAction: [],
+    who: ["Princess Anne", "High School Janitor", "Art Teacher", "Delivery Boy", "Elon Musk",
+    "RuPaul", "Lady Gaga", "Lunch Lady", "Sandwich Artist", "Shop Clerk",
+    "Boris Johnson", "TV Anchor", "Dentist", "Pediatrician", "Homeless Man",
+    "Bag Lady", "Experimental Theologian", "Priest", "Pope Francis", "Bus Driver",
+    "Grandma", "Retiree", "Police Chief", "Small Town Mayor", "Local Artist"],
+    
+    whatAction: ["eats", "grooms", "shoves", "sings about", "covets",
+    "ruins", "celebrates", "hugs", "makes amends with", "seranades",
+    "prays for", "performs autopsy on", "disects", "sits on", "elopes with",
+    "dances with", "underestimates", "makes a fool of", "exorcises", "laughs at",
+    "trips over", "sues", "hires", "apologizes to", "breastfeeds"],
+    
     whatAdj: [],
     whatObject: [],
     where: [],
